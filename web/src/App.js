e3595f30cef034eed65ffe834eec4c3e37db0d01
Jhonatan Uribe <jotauribe@Jhonatans-MacBook-Pro.local>
2022-10-19 00:23:23 -0500

    Creating movie list

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const { data } = await axios
+        .get("http://localhost:3001/movies")
+        .catch(() => setError("Error al obtener listado. Intente nuevamente"));
+      setMovies(data);
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <ul className="movies">
+        {error ? (
+          <Alert
+            severity="error"
+            action={
+              <Button color="inherit" size="small" onClick={() => setError("")}>
+                Aceptar
+              </Button>
+            }
+          >
+            This is a success alert — check it out!
+          </Alert>
+        ) : (
+          movies.map((movie) => (
+            <li>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                  title={movie.name}
+                  subheader={`${movie.type.toUpperCase()} - ${movie.year}`}
+                />
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={movie.posterImageUrl}
+                  alt="Poster"
+                />
+              </Card>
+            </li>
+          ))
+        )}
+      </ul>
+    </Container>
   );
 }
 
